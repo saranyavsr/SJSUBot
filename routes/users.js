0bb650f422fe795e057fd40194e4e9b151c4f688
Saranya <saranya.vsrajan@gmail.com>
2017-12-08 19:44:18 -0800

    Added Database credentials

--- routes/users.js
+++ routes/users.js
@@ -15,12 +15,95 @@ router.get('/login', function(req, res){
 	}
 });
 
-// Bot page / index 
+// Bot page / index
 router.get('/index', function(req, res){
 	res.render('index');
 });
 
+// Register
+router.get('/register', function(req, res){
+	res.render('register', {layout :'other'});
+});
 
+// Adminlogin
+router.get('/adminlogin', function(req, res){
+	console.log("Getting admin");
+	res.render('adminlogin', {layout :'other'});
+});
+
+// AdminloginPost
+router.post('/adminlogin', function(req, res){
+	console.log("Posting to admin");
+	var username = req.body.username;
+	var password = req.body.password;
+	if(username == "admin@sjsu.edu"){
+		console.log("Valid admin credentials");
+			res.render('register', {layout :'other'});
+	}
+
+	else
+		{
+			console.log("Invalid admin credentials");
+			req.flash('error_msg', 'Invalid Admin credentials');
+			//res.render('adminlogin', {layout :'other'});
+			res.redirect('adminlogin');
+		}
+
+});
+
+
+
+// Register User
+router.post('/register', function(req, res){
+	var name = req.body.name;
+	var email = req.body.email;
+	var password = req.body.password;
+	var student_id = req.body.student_id;
+	var courses_completed = req.body.courses_completed;
+	var courses_remaining = req.body.courses_remaining;
+	var credits = req.body.credits;
+	var due_assignments = req.body.due_assignments;
+	var due_fees = req.body.due_fees;
+	var gpa = req.body.gpa;
+	var upcoming_quizzes = req.body.upcoming_quizzes;
+
+	// Validation
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('password', 'Password is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register',{
+			errors:errors
+		});
+	} else {
+		var newUser = new User({
+			name: name,
+			email:email,
+			password: password,
+			student_id : student_id,
+			courses_completed : courses_completed,
+			courses_remaining : courses_remaining,
+			credits : credits,
+			due_assignments : due_assignments,
+			due_fees : due_fees,
+			gpa : gpa,
+			upcoming_quizzes : upcoming_quizzes
+		});
+
+		User.createUser(newUser, function(err, user){
+			if(err) throw err;
+			console.log(user);
+		});
+
+		req.flash('success_msg', 'You are registered and can now login');
+		// res.redirect('/users/login');
+		res.redirect('/');
+	}
+});
 
 
 passport.use(new LocalStrategy(
@@ -43,7 +126,7 @@ passport.use(new LocalStrategy(
   }));
 
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+  done(null, user);
 });
 
 passport.deserializeUser(function(id, done) {
@@ -60,8 +143,9 @@ router.post('/login', passport.authenticate('local', {successRedirect:'index', f
 
 router.get('/logout', function(req, res){
 	req.logout();
+	req.session.destroy();
 
-	req.flash('success_msg', 'You are logged out');
+
 
 	res.redirect('/users/login');
 });
